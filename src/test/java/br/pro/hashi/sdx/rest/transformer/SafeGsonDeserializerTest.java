3edd541dfd0399ac4ad1918b5fe51a8264290525
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2022-12-28 18:40:31 -0300

    Add safe gson deserializer

---
+++ src/test/java/br/pro/hashi/sdx/rest/transformer/SafeGsonDeserializerTest.java
@@ -0,0 +1,69 @@
+package br.pro.hashi.sdx.rest.transformer;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.mockStatic;
+import static org.mockito.Mockito.times;
+
+import java.math.BigInteger;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
+
+import com.google.gson.Gson;
+
+import br.pro.hashi.sdx.rest.base.Reflection;
+import br.pro.hashi.sdx.rest.transformer.base.Deserializer;
+
+class SafeGsonDeserializerTest {
+	MockedStatic<Reflection> reflection;
+	private Deserializer d;
+	private String content;
+
+	@BeforeEach
+	void setUp() {
+		reflection = mockStatic(Reflection.class);
+		d = new SafeGsonDeserializer(mock(Gson.class));
+		content = "content";
+	}
+
+	@AfterEach
+	void tearDown() {
+		reflection.close();
+	}
+
+	@Test
+	void addsObjectOnlyOnce() {
+		assertDoesNotThrow(() -> {
+			d.deserialize(content, Object.class);
+			d.deserialize(content, Object.class);
+		});
+		reflection.verify(() -> Reflection.getNoArgsConstructor(Object.class), times(1));
+	}
+
+	@Test
+	void doesNotAddPrimitive() {
+		assertDoesNotThrow(() -> {
+			d.deserialize(content, boolean.class);
+		});
+		reflection.verify(() -> Reflection.getNoArgsConstructor(int.class), times(0));
+	}
+
+	@Test
+	void doesNotAddWrapper() {
+		assertDoesNotThrow(() -> {
+			d.deserialize(content, boolean.class);
+		});
+		reflection.verify(() -> Reflection.getNoArgsConstructor(Integer.class), times(0));
+	}
+
+	@Test
+	void doesNotAddBig() {
+		assertDoesNotThrow(() -> {
+			d.deserialize(content, boolean.class);
+		});
+		reflection.verify(() -> Reflection.getNoArgsConstructor(BigInteger.class), times(0));
+	}
+}
