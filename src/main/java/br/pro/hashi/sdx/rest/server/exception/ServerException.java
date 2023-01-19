fb507e51eb5411c8af66ef13a6598e57b5cd4ec9
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-19 11:38:20 -0300

    Add server exception

---
+++ src/main/java/br/pro/hashi/sdx/rest/server/exception/ServerException.java
@@ -0,0 +1,9 @@
+package br.pro.hashi.sdx.rest.server.exception;
+
+public class ServerException extends RuntimeException {
+	private static final long serialVersionUID = -3706187038675186123L;
+
+	public ServerException(Throwable cause) {
+		super(cause);
+	}
+}
