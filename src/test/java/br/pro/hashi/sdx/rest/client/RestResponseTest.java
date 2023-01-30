39aeb2821a5c62e24f319f81bb6179e221e0705c
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2023-01-30 14:56:00 -0300

    Complete clienty

--- src/test/java/br/pro/hashi/sdx/rest/client/RestResponseTest.java
+++ src/test/java/br/pro/hashi/sdx/rest/client/RestResponseTest.java
@@ -1,14 +1,22 @@
 package br.pro.hashi.sdx.rest.client;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.InputStream;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import br.pro.hashi.sdx.rest.client.exception.ClientException;
 import br.pro.hashi.sdx.rest.reflection.Headers;
+import br.pro.hashi.sdx.rest.transform.Deserializer;
+import br.pro.hashi.sdx.rest.transform.Disassembler;
+import br.pro.hashi.sdx.rest.transform.Hint;
 import br.pro.hashi.sdx.rest.transform.facade.Facade;
 
 class RestResponseTest {
@@ -20,11 +28,76 @@ class RestResponseTest {
 	void setUp() {
 		facade = mock(Facade.class);
 		headers = mock(Headers.class);
-		r = new RestResponse(facade, 600, headers, "type/subtype", InputStream.nullInputStream());
+		r = new RestResponse(facade, 600, headers, null, InputStream.nullInputStream());
 	}
 
 	@Test
-	void stub() {
-		assertNotNull(r);
+	void getsBody() {
+		Object body = mockBody();
+		assertSame(body, r.getBody(Object.class));
+		assertThrows(ClientException.class, () -> {
+			r.getBody(Object.class);
+		});
+	}
+
+	@Test
+	void getsBodyWithHint() {
+		Object body = mockBody();
+		assertSame(body, r.getBody(new Hint<Object>() {}));
+		assertThrows(ClientException.class, () -> {
+			r.getBody(new Hint<Object>() {});
+		});
+	}
+
+	private Object mockBody() {
+		Object body = new Object();
+		Deserializer deserializer = mock(Deserializer.class);
+		when(deserializer.read(any(), eq(Object.class))).thenReturn(body);
+		when(facade.isBinary(Object.class)).thenReturn(false);
+		when(facade.cleanForDeserializing(null, Object.class)).thenReturn("type/subtype");
+		when(facade.getDeserializer("type/subtype")).thenReturn(deserializer);
+		return body;
+	}
+
+	@Test
+	void doesNotGetBody() {
+		assertThrows(NullPointerException.class, () -> {
+			r.getBody((Class<?>) null);
+		});
+	}
+
+	@Test
+	void doesNotGetBodyWithHint() {
+		assertThrows(NullPointerException.class, () -> {
+			r.getBody((Hint<?>) null);
+		});
+	}
+
+	@Test
+	void getsBinaryBody() {
+		Object body = mockBinaryBody();
+		assertSame(body, r.getBody(Object.class));
+		assertThrows(ClientException.class, () -> {
+			r.getBody(Object.class);
+		});
+	}
+
+	@Test
+	void getsBinaryBodyWithHint() {
+		Object body = mockBinaryBody();
+		assertSame(body, r.getBody(new Hint<Object>() {}));
+		assertThrows(ClientException.class, () -> {
+			r.getBody(new Hint<Object>() {});
+		});
+	}
+
+	private Object mockBinaryBody() {
+		Object body = new Object();
+		Disassembler disassembler = mock(Disassembler.class);
+		when(disassembler.read(any(), eq(Object.class))).thenReturn(body);
+		when(facade.isBinary(Object.class)).thenReturn(true);
+		when(facade.cleanForDisassembling(null, Object.class)).thenReturn("type/subtype");
+		when(facade.getDisassembler("type/subtype")).thenReturn(disassembler);
+		return body;
 	}
 }
